cfaf39c039c127395de01b98c92420ba3f6614bb
Graden <101433010+GramGra07@users.noreply.github.com>
2024-07-11 23:24:34 -0600

    final project completion :)

--- blynk/blynk.ino
+++ blynk/blynk.ino
@@ -16,8 +16,10 @@
 #define gPin 14
 #define bPin 12
 
-bool triggeredDistance = 0;
 bool hallTriggered = 0;
+
+bool notifyHall = 0;
+bool notifyDist = 0;
 
 int distanceLimit = 0;
 int distance = 0;
@@ -26,12 +28,10 @@ long duration;
 bool hallConnected = 0;
 bool pastHallConnected = 0;
 
-bool stealthMode = 0; //to add // how to turn off main light
-
-bool usingDistance = 0;
+bool stealthMode = 0;  //to add // how to turn off main light
 
 bool rgbOn = 1;
-int pastR = 0;
+int pastR = 255;
 int pastG = 0;
 int pastB = 0;
 
@@ -59,7 +59,7 @@ void connectWiFi() {
 
   while (attemptCount < maxAttempts) {
     WiFi.disconnect(true);  // Disconnect from any network
-    delay(1000);  // Wait for the disconnection to complete
+    delay(1000);            // Wait for the disconnection to complete
 
     // Serial.println("Trying to connect to Network1...");
     WiFi.begin(ssid1, pass1);
@@ -81,14 +81,14 @@ void connectWiFi() {
     WiFi.disconnect(true);  // Disconnect before trying the next network
     delay(1000);
 
-  // if (WiFi.status() != WL_CONNECTED) {
-  //   // Serial.println("Could not connect to any network. Rebooting...");
-  //   ESP.restart();
-  // }
+    // if (WiFi.status() != WL_CONNECTED) {
+    //   // Serial.println("Could not connect to any network. Rebooting...");
+    //   ESP.restart();
+    // }
   }
 }
-void setup()
-{
+bool didTrigger = 0;
+void setup() {
   // Debug console
   Serial.begin(115200);
   connectWiFi();
@@ -105,90 +105,79 @@ void setup()
   pinMode(bPin, OUTPUT);
 }
 
-BLYNK_CONNECTED()
-{
+BLYNK_CONNECTED() {
   Blynk.syncVirtual(V6);
   Blynk.syncVirtual(V7);
   Blynk.syncVirtual(V5);
-  Blynk.syncVirtual(V8);
+  Blynk.syncVirtual(V1);
+  Blynk.syncVirtual(V9);
+  Blynk.syncVirtual(V10);
 }
 
-BLYNK_WRITE(V0)
-{
+BLYNK_WRITE(V0) {
   int pinValue = param.asInt();
-  if (pinValue == 0 ||(!stealthMode && pinValue == 1)){
-  digitalWrite(MAIN_LED_PIN, pinValue);
-  }else if (stealthMode){
-  digitalWrite(MAIN_LED_PIN, 0);
+  if (pinValue == 0 || (!stealthMode && pinValue == 1)) {
+    digitalWrite(MAIN_LED_PIN, pinValue);
+  } else if (stealthMode) {
+    digitalWrite(MAIN_LED_PIN, 0);
   }
 }
-
-BLYNK_WRITE(V9){
-  triggeredDistance = 0;
-  Blynk.virtualWrite(V9,0);
+BLYNK_WRITE(V9) {
+  int val = param.asInt();
+  notifyDist = val;
 }
-BLYNK_WRITE(V5)
-{
+BLYNK_WRITE(V10) {
+  int val = param.asInt();
+  notifyHall = val;
+}
+BLYNK_WRITE(V5) {
   int val = param.asInt();
   distanceLimit = val;
 }
-BLYNK_WRITE(V6)
-{
+BLYNK_WRITE(V6) {
   int val = param.asInt();
   moreThan = val;
 }
 
-BLYNK_WRITE(V7)
-{
+BLYNK_WRITE(V7) {
   int val = param.asInt();
   rgbOn = val;
 }
 
-BLYNK_WRITE(V8){
-  int val = param.asInt();
-  usingDistance = val;
-}
-BLYNK_WRITE(V1){
+BLYNK_WRITE(V1) {
   int val = param.asInt();
   stealthMode = val;
   Blynk.syncVirtual(V0);
 }
-void loop()
-{
+void loop() {
   Blynk.run();
   readHallEffect();
-  if (usingDistance){  
-    readDistance();
-  }
+  readDistance();
   rgbOnOff();
+  Serial.println(pastG);
 }
 
-bool didTrigger = 0;
-void readHallEffect()
-{
+void readHallEffect() {
   hallConnected = !digitalRead(HALL_PIN);
-  if (hallConnected != pastHallConnected)
-  {
+  if (hallConnected != pastHallConnected) {
     pastHallConnected = hallConnected;
     Blynk.virtualWrite(V2, hallConnected);
   }
-  if (hallConnected)
-  {
+  if (hallConnected) {
     setRGB(0, 255, 0);
     hallTriggered = 0;
-  }
-  else
-  {
-    if (!hallTriggered){
-    Blynk.logEvent("hall_not_connected");
-    setRGB(255, 0, 0);
-    hallTriggered = 1;
+  } else {
+    if (hallTriggered == 0) {
+      if (notifyHall == 1) {
+        Blynk.logEvent("hall_not_connected");
+      }
+      hallTriggered = 1;
+      setRGB(255, 0, 0);
     }
   }
 }
 
-void readDistance()
-{
+void readDistance() {
   digitalWrite(TRIG_PIN, LOW);
   delayMicroseconds(2);
   digitalWrite(TRIG_PIN, HIGH);
@@ -196,40 +185,46 @@ void readDistance()
   digitalWrite(TRIG_PIN, LOW);
   duration = pulseIn(ECHO_PIN, HIGH);
   distance = duration * 0.034 / 2;
-  Blynk.virtualWrite(V4,distance);
-  if (moreThan == 1){
-    if (distance > distanceLimit){
-      didTrigger = 1;
+  Blynk.virtualWrite(V4, distance);
+  if (moreThan == 1) {
+    if (distance > distanceLimit) {
+      // Serial.println("more than");
+      if (didTrigger == 0) {
+        if (notifyDist == 1) {
+          Blynk.logEvent("distance_over_limit");
+        }
+        didTrigger = 1;
+      }
+    } else if (distance < distanceLimit) {
+      didTrigger = 0;
     }
-  }
-  if (moreThan == 0){
-    if (distance < distanceLimit){
-      didTrigger = 1;
+  } else if (moreThan == 0) {
+    if (distance < distanceLimit) {
+      // Serial.println("less than");
+      if (didTrigger == 0) {
+        if (notifyDist == 1) {
+          Blynk.logEvent("distance_under_limit");
+        }
+        didTrigger = 1;
+      }
+    } else if (distance > distanceLimit) {
+      didTrigger = 0;
     }
-  }
-  if (didTrigger == 1 && triggeredDistance == 0){
-    triggeredDistance = 1;
-    Blynk.logEvent("distance_under_limit");
   }
 }
 
-void rgbOnOff()
-{
-  if (rgbOn && !stealthMode)
-  {
+void rgbOnOff() {
+  if (rgbOn == 1 && stealthMode == 0) {
     analogWrite(rPin, pastR);
     analogWrite(gPin, pastG);
     analogWrite(bPin, pastB);
-  }
-  else
-  {
+  } else {
     analogWrite(rPin, 0);
     analogWrite(gPin, 0);
     analogWrite(bPin, 0);
   }
 }
-void setRGB(int r, int g, int b)
-{
+void setRGB(int r, int g, int b) {
   pastR = r;
   pastG = g;
   pastB = b;
